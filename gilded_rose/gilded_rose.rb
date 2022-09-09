9847b2b64f7f1e83d00380919f29f86e849c8c20
Kevin <kevinmulhern23@gmail.com>
2022-09-09 17:02:38 +0100

    Completed Exercise

--- gilded_rose/gilded_rose.rb
+++ gilded_rose/gilded_rose.rb
@@ -1,55 +1,24 @@
 class GildedRose
-
   def initialize(items)
-    @items = items
+    @items = items.map { |item| item_mapper(item) }
   end
 
   def update_quality()
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+      item.set_quality
+      item.set_sell_in
     end
+  end
+
+  private
+
+  def item_mapper(item)
+    {
+      "Aged Brie" => AgedBrie.new(item),
+      "Sulfuras, Hand of Ragnaros" => Sulfuras.new(item),
+      "Backstage passes to a TAFKAL80ETC concert" => BackstagePasses.new(item),
+      "Conjured" => Conjured.new(item)
+    }.fetch(item.name, Normal.new(item))
   end
 end
 
@@ -64,5 +33,89 @@ class Item
 
   def to_s()
     "#{@name}, #{@sell_in}, #{@quality}"
+  end
+end
+
+class AgedBrie
+  def initialize(item)
+    @item = item
+  end
+
+  def set_quality
+    return if @item.quality >= 50
+
+    @item.quality += 1
+    @item.quality += 1 if @item.sell_in <= 0
+  end
+
+  def set_sell_in
+    @item.sell_in -= 1
+  end
+
+end
+
+class Sulfuras
+  def initialize(item)
+    @item = item
+  end
+
+  def set_quality
+  end
+
+  def set_sell_in
+    @item.sell_in -= 0
+  end
+end
+
+class BackstagePasses
+  def initialize(item)
+    @item = item
+  end
+
+  def set_quality
+    return if @item.quality >= 50 || @item.quality <= 0
+
+    @item.quality += 1
+    @item.quality += 1 if @item.sell_in <= 10
+    @item.quality += 1 if @item.sell_in <= 5
+    @item.quality -= @item.quality if @item.sell_in <= 0
+  end
+
+  def set_sell_in
+    @item.sell_in -= 1
+  end
+end
+
+class Normal
+  def initialize(item)
+    @item = item
+  end
+
+  def set_quality
+    return if @item.quality <= 0
+
+    @item.quality -= 1
+    @item.quality -= 1 if @item.sell_in < 0
+  end
+
+  def set_sell_in
+    @item.sell_in -= 1
+  end
+end
+
+class Conjured
+  def initialize(item)
+    @item = item
+  end
+
+  def set_quality
+    return if @item.quality >= 50 || @item.quality <= 0
+
+    @item.quality -= 2
+    @item.quality -= 2 if @item.sell_in <= 0
+  end
+
+  def set_sell_in
+    @item.sell_in -= 1
   end
 end
--- gilded_rose/gilded_rose_spec.rb
+++ gilded_rose/gilded_rose_spec.rb
@@ -3,11 +3,357 @@ require File.join(File.dirname(__FILE__), 'gilded_rose')
 describe GildedRose do
 
   describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
+
+    describe "normal item" do
+      context "before sell date" do
+        it "decreases quality by 1" do
+          items = [Item.new("normal", 1, 2)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(1)
+        end
+
+        it "cannot decrease quality below 0" do
+          items = [Item.new("normal", 10, 0)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(0)
+        end
+
+        it "decreases sell_in by 1" do
+          items = [Item.new("normal", 10, 10)]
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to eq(9)
+        end
+      end
+
+      context "on sell date" do
+        it "decreases quality by 1" do
+          items = [Item.new("normal", 0, 2)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(1)
+        end
+
+        it "cannot decrease quality below 0" do
+          items = [Item.new("normal", 0, 0)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(0)
+        end
+
+        it "decreases sell_in by 1" do
+          items = [Item.new("normal", 10, 10)]
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to eq(9)
+        end
+      end
+
+      context "after sell date" do
+        it "decreases quality by 2" do
+          items = [Item.new("normal", -1, 2)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(0)
+        end
+
+        it "cannot decrease quality below 0" do
+          items = [Item.new("normal", -1, 0)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(0)
+        end
+
+        it "decreases sell_in by 1" do
+          items = [Item.new("normal", 10, 10)]
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to eq(9)
+        end
+      end
+    end
+
+    describe "Aged Brie" do
+      context "before sell date" do
+        it "increases quality by 1" do
+          items = [Item.new("Aged Brie", 3, 0)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(1)
+        end
+
+        it "cannot increase in quality beyond 50" do
+          items = [Item.new("Aged Brie", 1, 50)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(50)
+        end
+      end
+
+      context "on sell date" do
+        it "increases quality twice as fast" do
+          items = [Item.new("Aged Brie", 0, 0)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(2)
+        end
+
+        it "cannot increase in quality beyond 50" do
+          items = [Item.new("Aged Brie", 0, 50)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(50)
+        end
+      end
+
+      context "after sell date" do
+        it "increases in quality twice as fast" do
+          items = [Item.new("Aged Brie", -1, 0)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(2)
+        end
+
+        it "cannot increase in quality beyond 50" do
+          items = [Item.new("Aged Brie", -1, 50)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(50)
+        end
+      end
+
+      it "increases in quality the older it gets" do
+        items = [Item.new("Aged Brie", 6, 10)]
+        GildedRose.new(items).update_quality
+        expect(items[0].quality).to eq(11)
+
+        items = [Item.new("Aged Brie", 4, 10)]
+        GildedRose.new(items).update_quality
+        expect(items[0].quality).to eq(11)
+      end
+
+      it "increases in quality twice as fast after sell by date" do
+        items = [Item.new("Aged Brie", -2, 8)]
+        GildedRose.new(items).update_quality
+        expect(items[0].quality).to eq(10)
+      end
+
+      it "cannot have a higher quality than 50" do
+        items = [Item.new("Aged Brie", -1, 50)]
+        GildedRose.new(items).update_quality
+        expect(items[0].quality).to eq(50)
+      end
+
+      it "decreases sell in days" do
+        items = [Item.new("Aged Brie", 10, 50)]
+        GildedRose.new(items).update_quality
+        expect(items[0].sell_in).to eq(9)
+      end
+    end
+
+    describe "Sulfuras" do
+      context "before sell by date" do
+        it "never has to be sold" do
+          items = [Item.new("Sulfuras, Hand of Ragnaros", 10, 50)]
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to eq(10)
+        end
+
+        it "never decreases in quality" do
+          items = [Item.new("Sulfuras, Hand of Ragnaros", 10, 50)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(50)
+        end
+      end
+
+      context "on sell by date" do
+        it "never has to be sold" do
+          items = [Item.new("Sulfuras, Hand of Ragnaros", 0, 50)]
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to eq(0)
+        end
+
+        it "never decreases in quality" do
+          items = [Item.new("Sulfuras, Hand of Ragnaros", 0, 50)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(50)
+        end
+      end
+
+      context "after sell by date" do
+        it "never has to be sold" do
+          items = [Item.new("Sulfuras, Hand of Ragnaros", -1, 50)]
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to eq(-1)
+        end
+
+        it "never decreases in quality" do
+          items = [Item.new("Sulfuras, Hand of Ragnaros", -1, 50)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(50)
+        end
+
+        it "cannot have a higher quality than 50" do
+          items = [Item.new("Aged Brie", -1, 50)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(50)
+        end
+      end
+    end
+
+    describe "Backstage passes" do
+      context "when sell in days is more than 10" do
+        it "increases in quality by 1" do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 10)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(11)
+        end
+
+        it "cannot have a higher quality than 50" do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 50)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(50)
+        end
+
+        it "cannot have a lower quality than 0" do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 0)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(0)
+        end
+
+        it "decreases sell in days" do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 50)]
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to eq(14)
+        end
+      end
+
+      context "when sell in days is 10 or less" do
+        it "increases in quality by 2" do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 10)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(12)
+        end
+
+        it "cannot have a higher quality than 50" do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 50)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(50)
+        end
+
+        it "cannot have a lower quality than 0" do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 0)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(0)
+        end
+
+        it "decreases sell in days" do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 50)]
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to eq(9)
+        end
+      end
+
+      context "when sell in days is 5 or less" do
+        it "increases in quality by 3" do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 10)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(13)
+        end
+
+        it "cannot have a higher quality than 50" do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 50)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(50)
+        end
+
+        it "cannot have a lower quality than 0" do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 0)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(0)
+        end
+
+        it "decreases sell in days" do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 5, 50)]
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to eq(4)
+        end
+      end
+
+      context "when sell in days is 0 or less" do
+        it "drops quality to 0" do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 10)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(0)
+        end
+
+        it "cannot have a higher quality than 50" do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 50)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(50)
+        end
+
+        it "cannot have a lower quality than 0" do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 0)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(0)
+        end
+
+        it "decreases sell in days" do
+          items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 50)]
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to eq(-1)
+        end
+      end
+    end
+
+    describe "Conjured" do
+      context "before sell by date" do
+        it "decreases in quality twice as fast as normal" do
+          items = [Item.new("Conjured", 10, 10)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(8)
+        end
+
+        it "cannot decrease quality below 0" do
+          items = [Item.new("Conjured", 5, 0)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(0)
+        end
+
+        it "decreases sell in days" do
+          items = [Item.new("Conjured", 10, 50)]
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to eq(9)
+        end
+      end
+
+      context "on sell by date" do
+        it "decreases in quality four times as fast as normal" do
+          items = [Item.new("Conjured", 0, 10)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(6)
+        end
+
+        it "cannot decrease quality below 0" do
+          items = [Item.new("Conjured", 0, 0)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(0)
+        end
+
+        it "decreases sell in days" do
+          items = [Item.new("Conjured", 0, 50)]
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to eq(-1)
+        end
+      end
+
+      context "after sell by date" do
+        it "decreases in quality four times as fast as normal" do
+          items = [Item.new("Conjured", -1, 10)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(6)
+        end
+
+        it "cannot decrease quality below 0" do
+          items = [Item.new("Conjured", -1, 0)]
+          GildedRose.new(items).update_quality
+          expect(items[0].quality).to eq(0)
+        end
+
+        it "decreases sell in days" do
+          items = [Item.new("Conjured", -1, 50)]
+          GildedRose.new(items).update_quality
+          expect(items[0].sell_in).to eq(-2)
+        end
+      end
     end
   end
-
 end
