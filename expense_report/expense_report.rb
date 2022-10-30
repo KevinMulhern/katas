896904a372a9ce96bed5aa308a30acdf76184774
Kevin <kevinmulhern23@gmail.com>
2022-10-30 09:42:51 +0000

    WIP: Add lunch expense

--- expense_report/expense_report.rb
+++ expense_report/expense_report.rb
@@ -54,6 +54,33 @@ class BreakfastExpense
   end
 end
 
+class LunchExpense
+  LIMIT = 2000
+  attr_reader :amount
+
+  def initialize(amount)
+    @amount = amount
+  end
+
+  def to_s
+    "#{name}\t#{amount}\t#{over_limit? ? 'X' : ' '}"
+  end
+
+  def meal?
+    true
+  end
+
+  private
+
+  def name
+    "Lunch"
+  end
+
+  def over_limit?
+    amount > LIMIT
+  end
+end
+
 class CarRentalExpense
   attr_reader :amount
 
--- expense_report/expense_report_spec.rb
+++ expense_report/expense_report_spec.rb
@@ -25,7 +25,7 @@ RSpec.describe ExpenseReport do
       ).to_stdout
     end
 
-    context "when meal expense is over 5000" do
+    context "when meal expenses are over their limits" do
       it "prints the report" do
         expense_one = DinnerExpense.new(6000)
         expense_two = BreakfastExpense.new(7000)
@@ -42,6 +42,56 @@ RSpec.describe ExpenseReport do
 
         expect {
           described_class.new([expense_one, expense_two, expense_three]).to_s
+        }.to output(
+          expected_output
+        ).to_stdout
+      end
+    end
+
+    context "with lunch expense" do
+      it "prints the report" do
+        breakfast = BreakfastExpense.new(1000)
+        lunch = LunchExpense.new(1000)
+        dinner = DinnerExpense.new(3000)
+        car_rental = CarRentalExpense.new(5000)
+
+        expected_output = <<~EXPENSE_REPORT
+          Expenses: #{Time.now}
+          Breakfast\t1000\t\s
+          Lunch\t1000\t\s
+          Dinner\t3000\t\s
+          Car Rental\t5000\t\s
+          Meal Expenses: 5000
+          Total Expenses: 10000
+        EXPENSE_REPORT
+
+        expect {
+          described_class.new([breakfast, lunch, dinner, car_rental]).to_s
+        }.to output(
+          expected_output
+        ).to_stdout
+      end
+    end
+
+    context "with over limit lunch expense" do
+      it "prints the report" do
+        breakfast = BreakfastExpense.new(1000)
+        lunch = LunchExpense.new(3000)
+        dinner = DinnerExpense.new(3000)
+        car_rental = CarRentalExpense.new(5000)
+
+        expected_output = <<~EXPENSE_REPORT
+          Expenses: #{Time.now}
+          Breakfast\t1000\t\s
+          Lunch\t3000\tX
+          Dinner\t3000\t\s
+          Car Rental\t5000\t\s
+          Meal Expenses: 7000
+          Total Expenses: 12000
+        EXPENSE_REPORT
+
+        expect {
+          described_class.new([breakfast, lunch, dinner, car_rental]).to_s
         }.to output(
           expected_output
         ).to_stdout
